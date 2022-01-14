d269d36b2e5af1ddf0d1df636ed262fef4a6c03d
C5m7b4 <mbedingfield@dcrpos.com>
2022-01-14 15:00:26 -0600

    add ComplexComponent

--- example/src/Demo.tsx
+++ example/src/Demo.tsx
@@ -6,6 +6,23 @@ import '../../dist/css/c5-tooltip.css';
 
 import './Demo.css';
 
+const ComplexComponent = () => {
+  return (
+    <div style={{ borderRadius: '10px' }}>
+      <div
+        style={{
+          borderBottom: '2px solid black',
+          fontSize: '1.2rem',
+          fontWeight: 'bold',
+        }}
+      >
+        Title
+      </div>
+      <div>Here is the body of my tooltip</div>
+    </div>
+  );
+};
+
 export const Demo: React.FC = () => {
   const [open, isOpen] = useState(false);
   const [direction, setDirection] = useState<Direction>('top');
@@ -19,9 +36,9 @@ export const Demo: React.FC = () => {
         <div className="demo">
           <ul>
             <li>List element-1</li>
-            <ToolTipController direction={direction} delay={delay}>
+            {/* <ToolTipController direction={direction} delay={delay}>
               <li tool-tip="Hello my name is mike">List Element-2</li>
-            </ToolTipController>
+            </ToolTipController> */}
             <li>List element-3</li>
           </ul>
         </div>
@@ -31,6 +48,27 @@ export const Demo: React.FC = () => {
           <Tooltip content="hello" direction="right">
             <button>Im a button</button>
           </Tooltip>
+        </div>
+        <div className="advanced">
+          <ToolTipController direction={direction} delay={delay}>
+            <p>Here is just some text that does nothing</p>
+            <button className="advanced-button" tool-tip="Hey, dont press me">
+              Press Me
+            </button>
+            <ul>
+              <li>This item does nothing</li>
+              <li tool-tip="This line has a tooltip">
+                This line does something
+              </li>
+              <li>This line does nothing</li>
+              <li tool-tip="Hey, this one does something too">
+                This one does something
+              </li>
+            </ul>
+            <div tool-tip={<ComplexComponent />}>
+              This div should show a complex component
+            </div>
+          </ToolTipController>
         </div>
 
         <div className="controls">
