94e58149b2f4d3a48a1745da4781b74ed7806918
C5m7b4 <mbedingfield@dcrpos.com>
2022-01-18 11:56:28 -0600

    add ToolTipController as default export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
+import ToolTipController from './core/Controller/ToolTipController';
 export * from './components';
 export * from './core';
 export * from './utils';
 export * from './hooks';
 export * from './types';
 export * from './interfaces';
+
+export default ToolTipController;
